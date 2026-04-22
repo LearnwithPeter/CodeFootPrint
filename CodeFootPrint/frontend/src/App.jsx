ab677f036e069845b6e1e54cbda238c807631044
malhotraharsh241 <malhotraharsh241@gmail.com>
2026-04-22 20:13:19 +0530

    Routes are updated

--- CodeFootPrint/frontend/src/App.jsx
+++ CodeFootPrint/frontend/src/App.jsx
@@ -1,18 +1,19 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import Home from "./pages/Home";
-import About from "./pages/About";
+import Home    from "./pages/Home";
+import About   from "./pages/About";
 import Contact from "./pages/Contact";
+import Analyze from "./pages/Analyze";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Navbar />
-
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
+        <Route path="/"        element={<Home    />} />
+        <Route path="/about"   element={<About   />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/analyze" element={<Analyze />} />
       </Routes>
     </BrowserRouter>
   );
